dbb14dbb8e5eeea4f52224f69060c9d77f34d9f2
kanon755056 <32713752+kanon755056@users.noreply.github.com>
2025-11-09 01:20:48 +0000

    build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',                    // GitHub Pages 必須加這行
+  base: '/te/',   // ← 這裡改成你庫的真實名稱！（例如 my-idle-game）
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
