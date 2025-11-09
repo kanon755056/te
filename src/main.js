83e853285e6e783c2760375e45cc1740f3611257
kanon755056 <32713752+kanon755056@users.noreply.github.com>
2025-11-09 01:44:46 +0000

    build

--- src/main.js
+++ src/main.js
@@ -4,3 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// 每秒執行一次
+setInterval(() => {
+    con()
+}, 1000)
+
+const con = () => console.log('test 1s')
